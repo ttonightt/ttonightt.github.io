efdfc6d6f76dbd2faff89f5fae65ca8bed9537dd
ttonightt <ttonightt@outlook.com>
2025-09-17 18:30:56 +0200

    Added class prop to Scale, Done sticky behavior of layout

--- src/App.vue
+++ src/App.vue
@@ -11,62 +11,81 @@ const setTabIndex = index => tabIndex.value = index;
 </script>
 
 <template>
-	<div class="bg-gray-900 w-screen h-screen max-h-screen overflow-hidden flex items-center justify-center text-gray-300">
-		<div class="flex align-start gap-7 py-10h max-h-full">
-			<div class="flex flex-col w-15w items-end">
+	<div class="bg-gray-900 w-screen min-h-screen flex items-center justify-center text-gray-300">
+		<div class="flex gap-7">
+			<div class="py-10h flex flex-col w-15w items-end sticky top-0 self-start">
 				<Emblem color="#fff" />
 				<h1 v-if="tabIndex === 0">Mykhailo Astashenkov</h1>
 				<h3 v-if="tabIndex !== 0" class="h3 text-right">Mykhailo Astashenkov</h3>
 				<h5 class="text-right">Full stack</h5>
 				<h5 class="text-right">Web development enthusiast from Slovenia</h5>
 			</div>
-			<div class="w-0.5 bg-gray-700 basis-0.5 shrink-0" :class="tabIndex === 0 ? 'hidden' : ''"></div>
+			<div class="w-0.5 max-h-screen sticky top-0 grad basis-0.5 shrink-0" :class="tabIndex === 0 ? 'hidden' : ''"></div>
 			<TabGroup @change="setTabIndex">
-				<TabPanels class="w-40w overflow-y-scroll overflow-x-visible" :class="tabIndex === 0 ? 'hidden' : ''">
+				<TabPanels class="py-10h w-40w" :class="tabIndex === 0 ? 'hidden' : ''">
 					<TabPanel></TabPanel>
 					<TabPanel>
 						<h3>React.js</h3>
-						<Scale steps="3" value="2" />
+						<Scale steps="3" value="2" class="w-36" />
 						<h3>Vue</h3>
-						<Scale steps="3" value="2" />
+						<Scale steps="3" value="2" class="w-36" />
 						<h3>Tailwind CSS v3</h3>
-						<Scale steps="3" value="2" />
+						<Scale steps="3" value="2" class="w-36" />
 						<h3>Tailwind CSS v4</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Express.js</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>bcrypt</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>jwt</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Redux</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Tanstack (React) Query</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Joy UI</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Headless UI</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Vitest</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Playwright</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Mongo DB</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>Mongoose</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>WebGL</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
 						<h3>three.js</h3>
-						<Scale steps="3" value="1" />
+						<Scale steps="3" value="1" class="w-36" />
+
+						<h3>Joy UI</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>Headless UI</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>Vitest</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>Playwright</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>Mongo DB</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>Mongoose</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>WebGL</h3>
+						<Scale steps="3" value="1" class="w-36" />
+						<h3>three.js</h3>
+						<Scale steps="3" value="1" class="w-36" />
 					</TabPanel>
 					<TabPanel>Projects</TabPanel>
 				</TabPanels>
-				<div class="w-0.5 bg-gray-700 basis-0.5 shrink-0"></div>
-				<TabList class="flex align-start justify-center flex-col gap-2 *:bg-gray-800 *:rounded-full *:py-1 *:px-3 *:w-fit *:cursor-pointer">
-					<Tab>About</Tab>
-					<Tab>Skills</Tab>
-					<Tab>Projects</Tab>
+				<div class="w-0.5 grad max-h-screen sticky top-0 grad basis-0.5 shrink-0"></div>
+				<TabList class="max-h-screen flex items-center">
+					<div class="flex flex-col sticky top-10h gap-2 *:bg-gray-800 *:rounded-full *:py-1 *:px-3 *:w-fit *:cursor-pointer">
+						<Tab>Me myself</Tab>
+						<Tab>What do I know</Tab>
+						<Tab>What have I done</Tab>
+					</div>
 				</TabList>
 			</TabGroup>
 		</div>
--- src/templates/Scale.vue
+++ src/templates/Scale.vue
@@ -1,18 +1,17 @@
 <script setup>
 
-const props = defineProps({
-	steps: Number,
-	value: Number
-});
+const props = defineProps([ "steps", "value", "class" ]);
 </script>
 
 <template>
-	<div class="relative flex items-center justify-between w-36 h-1 rounded-full bg-gray-800 *:rounded-full">
-		<div
-			v-for="i in parseInt(steps)"
-			class="size-3 -mx-1.5"
-			:class="i <= parseInt(value) ? 'bg-gray-50' : 'bg-gray-800'"
-		></div>
-		<div class="absolute h-full bg-gray-50" :style="{ width: 100 * (value - 1) / (steps - 1) + '%' }"></div>
+	<div class="mx-1.5 my-1" :class="props.class">
+		<div class="relative flex items-center justify-between w-full h-1 rounded-full bg-gray-800 *:rounded-full">
+			<div
+				v-for="i in parseInt(steps)"
+				class="size-3 -mx-1.5"
+				:class="i <= parseInt(value) ? 'bg-gray-50' : 'bg-gray-800'"
+			></div>
+			<div class="absolute h-full bg-gray-50" :style="{ width: 100 * (value - 1) / (steps - 1) + '%' }"></div>
+		</div>
 	</div>
 </template>
